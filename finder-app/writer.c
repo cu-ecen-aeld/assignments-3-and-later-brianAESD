019ccaa4e32e9fd298898a4a00aafda5c8f3c185
Brian <brian.wan@colorado.edu>
2022-08-28 01:30:08 -0500

    Add fileresult and clean up writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -46,33 +46,3 @@ int main(int argc, char **argv)
 
    return 0;
 }
-
-#if 0
-
-set -e
-set -u
-
-if [ $# -lt 2 ]
-then
-	echo "Failed: Parameter(s) not specified"
-	exit 1
-fi	
-
-# First argument is full path to a file (including filename) on the filesystem
-Writefile=$1
-# Second argument is a text string which will be written within this file
-Writestr=$2
-
-mkdir -p  "$(dirname ${Writefile})" #newFolder2/test.txt
-if [ $? -ne 0 ]; then
-   echo "Failed to create."
-   exit 1
-fi
-
-echo "${Writestr}" > "${Writefile}"
-if [ $? -ne 0 ]; then
-   echo "Failed to write."
-   exit 1
-fi
-
-#endif
