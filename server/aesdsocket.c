b95a34b193fa475f8c3a4256cbfa9ddea2f7cddc
Brian <brian.wan@colorado.edu>
2022-09-15 23:40:10 -0500

    Assignment 5 Part 1 aesdsocket changes2

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -14,9 +14,6 @@
 #include <fcntl.h>
 
 
-
-#define MAX_MESSAGE_SIZE 1024
-
 #define PORT_TARGET "9000"
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 //#define DATA_FILE "mytest.txt"
@@ -27,13 +24,12 @@ struct addrinfo hints;
 
 bool caught_sig = false;
 int sockfd = -1;
-//int fp;
-FILE* fp;
+FILE* fp = 0;
 
-void connection_closed_procedure(void)
-{
-    //remove(DATA_FILE);
-}
+// void connection_closed_procedure(void)
+// {
+//     //remove(DATA_FILE);
+// }
 
 void exit_procedure(void)
 {
@@ -103,6 +99,12 @@ int main(int argc, char **argv)
         exit_procedure();
     }
 
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+    {
+        syslog(LOG_ERR, "Failed SO_REUSEADDR");
+        exit_procedure();
+    }
+
     // bind
     status = bind(sockfd, addrinfo_res->ai_addr, addrinfo_res->ai_addrlen);
     if (status == -1)
@@ -111,14 +113,11 @@ int main(int argc, char **argv)
         exit_procedure();
     }
 
-    // listen
-    status = listen(sockfd, MAX_NUM_CONNECTION);
-    if (status == -1)
+    if (addrinfo_res)
     {
-        syslog(LOG_ERR, "Failed to listen");
-        exit_procedure();
+        freeaddrinfo(addrinfo_res);
+        addrinfo_res = 0;
     }
-
 
     // -D: run the aesdsocket application as a daemon
     
@@ -128,8 +127,22 @@ int main(int argc, char **argv)
         pid_t childPid = fork();
         if (childPid == -1)
         {
-            exit_procedure();
+            printf("Failed to fork\n");
+            exit(-1);
         }
+        else if (childPid)
+        {
+            printf("Fork %d\n", childPid);
+            exit(0);
+        }
+    }
+
+    // listen
+    status = listen(sockfd, MAX_NUM_CONNECTION);
+    if (status == -1)
+    {
+        syslog(LOG_ERR, "Failed to listen");
+        exit_procedure();
     }
 
     // int returnState = 0;
@@ -159,11 +172,7 @@ int main(int argc, char **argv)
 
         // Process data
 
-#if 1
         fp = fopen(DATA_FILE,"a+");
-        //FILE* fp_send = fp;
-        //fp = fopen(DATA_FILE,"w+");
-        //if (fp == -1)
         if (fp == NULL)
         {
             printf("Failed: Unable to open file.\n");
@@ -171,50 +180,37 @@ int main(int argc, char **argv)
             exit(-1);
         }
         printf("Opened: %s\n", DATA_FILE);
-#endif
 
         bool connectionDone = 0;
         unsigned int receivedByteCount = 0;
-        
-        //unsigned int readRetryCnt = 0;
 
         while (connectionDone == 0)
         {
-
-
             int receiveResult = 0;
-            const unsigned int receiveDataSize = 100;// 128;
+            const unsigned int receiveDataSize = 100;
             char receiveData[40000];
             memset(receiveData, 0, 40000);
-        if (1)
-        {
+
             // receive
             receiveResult = recv(new_conn_socket, receiveData, receiveDataSize, 0);
-            //send(new_conn_socket, "abcdefg\n", 8, 0);
             if (receiveResult == -1)
             {
                 // unsuccessful
                 printf("Receive is unsuccessful.\n");
-                connection_closed_procedure();
-                //break;
+                //connection_closed_procedure();
             }
             else if (receiveResult == 0)
             {
-                // // connection is closed
-                // syslog(LOG_DEBUG, "Closed connection from %s\n", conn_ip);
-                // // to accept new connection
-                // connection_closed_procedure();
+                // connection is closed
+                syslog(LOG_DEBUG, "Closed connection from %s\n", conn_ip);
 
                 // Receive is done.
                 printf("Receive is done.\n");
                 connectionDone = 1;
-
-                //break;
             }
             else
             {
-                // ok
-                //readRetryCnt = 0;
+                // Received data
                 receivedByteCount += receiveResult;
                 printf("Received (%d): %s", receiveResult, receiveData);
                 bwanTest1++;
@@ -223,74 +219,27 @@ int main(int argc, char **argv)
                 fseek(fp, 0, SEEK_END);
                 fwrite(receiveData, sizeof(char), receiveResult, fp);
             }
-        }
 
-
-
-        //printf("send is next.\n");
-        
-        //while (1)
-        if (receivedByteCount)
-        {
             // send
-            char sendData[40000];
-            memset(sendData, 0, 40000);
-            //if (fp <= 0) printf("what");
-            // Seek to the beginning of the file
-            fseek(fp, 0, SEEK_SET);
-            //fread(sendData, 1, receiveResult, fp + sendSizeTotal);
-            //fread(sendData, 1, sendSizeTotal + receiveResult, fp);
-            fread(sendData, 1, sendSizeTotal + receivedByteCount, fp);
-            if (sendData[sendSizeTotal + receivedByteCount - 1] != '\n')
+            if (receivedByteCount)
             {
-                printf("No newline.\n");
-                continue;
+                char sendData[40000];
+                memset(sendData, 0, 40000);
+                // Seek to the beginning of the file
+                fseek(fp, 0, SEEK_SET);
+                fread(sendData, 1, sendSizeTotal + receivedByteCount, fp);
+                if (sendData[sendSizeTotal + receivedByteCount - 1] != '\n')
+                {
+                    printf("No newline.\n");
+                    continue;
+                }
+                printf("Sending  (%d): %s\n", sendSizeTotal + receivedByteCount, sendData);
+                int sendResult;
+                sendResult = send(new_conn_socket, sendData, sendSizeTotal + receivedByteCount, 0);
+                sendSizeTotal += receivedByteCount;
+                (void) sendResult;
             }
-            //if (dataSize < 64) { int size = dataSize; printf("Send (%d): %s", size, sendData); }
-            //int sendResult = send(new_conn_socket, sendData, 8, 0);
-            //printf("Sending  (%d): %s\n", sendSizeTotal + receiveResult, sendData);
-            printf("Sending  (%d): %s\n", sendSizeTotal + receivedByteCount, sendData);
-            int sendResult;
-            //int 
-            //sendResult = send(new_conn_socket, receiveData, 8, 0);
-            //if (bwanTest1 == 2)
-            //sendResult = send(new_conn_socket, "hijklmnop\n", 10, 0);
-            //else
-            //sendResult = send(new_conn_socket, sendData, sendSizeTotal + receiveResult, 0);
-            sendResult = send(new_conn_socket, sendData, sendSizeTotal + receivedByteCount, 0);
-            sendSizeTotal += receivedByteCount;
-            //printf("sending.\n");
-            //while (1)
-            {
-            //sendResult = send(new_conn_socket, "qwertyu\n", 7, 0);
-        //while(1);
-            //if (sendResult) break;
-            }
-            //printf("sent.\n");
-            //printf("Sent (%d): %s\n", sendResult, receiveData);
-            //int sendResult = 0;//send(new_conn_socket, sendData, 1, 0);
-            // if (sendResult == -1)
-            // {
-            //     // error
-            // }
-            //printf("Send (%d) end", sendResult);
-            (void) sendResult;
-            (void) sendData;
-            (void) bwanTest1;
-            //printf("Send is done\n");
-            //send(new_conn_socket, "\n", 1, 0); //bwan del
-        } //while (1);
-        // else if (connectionDone)
-        // {
-        //     readRetryCnt++;
-        //     if (readRetryCnt < 5)
-        //     {
-        //         // retry
-        //         connectionDone = 0;
-        //     }
-        // }
 
-        
         }
         printf("new connection is done.\n");
         close(new_conn_socket);
@@ -298,52 +247,9 @@ int main(int argc, char **argv)
         fclose(fp);
         fp = 0;
 
-
-
-        (void) new_conn_socket;
     }
 
 
-
-
-#if 0
-   if (argc != 3)
-   {
-      printf("Failed: Number of parameter (%d) not valid\n", argc-1);
-      syslog(LOG_ERR, "Failed: Number of parameter (%d) not valid\n", argc-1);
-      exit(1);
-   }
-
-   // First argument is full path to a file (including filename) on the filesystem
-   char* writefile = argv[1];
-   // Second argument is a text string which will be written within this file
-   char* writestr = argv[2];
-   unsigned int writeStrLen = strlen(writestr);
-
-   printf("Writing %s to %s\n", writestr, writefile);
-   syslog(LOG_DEBUG, "Writing %s to %s\n", writestr, writefile);
-   FILE *fp=fopen(writefile,"wb");
-   if (fp == NULL)
-   {
-      printf("Failed: Unable to open file.\n");
-      syslog(LOG_ERR, "Failed: Unable to open file.\n");
-      exit(1);
-   }
-
-   unsigned int numWritten = fwrite(writestr, 1, writeStrLen, fp);
-   if (numWritten != writeStrLen)
-   {
-      printf("Failed: Unable to write %d bytes. Wrote %d bytes.\n", writeStrLen, numWritten);
-      syslog(LOG_ERR, "Failed: Unable to write %d bytes. Wrote %d bytes.\n", writeStrLen, numWritten);
-      exit(1);
-   }
-   printf("Wrote string.\n");
-
-   fflush(fp);
-   fclose(fp);
-#endif
-
-    //exit_procedure();
     printf("End.\n");
     return 0;
 }
